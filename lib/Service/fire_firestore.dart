e2fd9ccf4cdf6a315af77fb5a5b3d803b04e2a1d
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-10-15 18:58:27 +0200

    clear functions

--- lib/Service/fire_firestore.dart
+++ lib/Service/fire_firestore.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart' hide Order;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ykos_kitchen/Service/fire_auth.dart';
-import 'package:ykos_kitchen/model/adress.dart';
-import 'package:ykos_kitchen/model/food.dart';
 import 'package:ykos_kitchen/model/order.dart';
 
 class FireFirestore {
@@ -18,88 +16,10 @@ class FireFirestore {
         message: 'Kein Benutzer ist derzeit eingeloggt.',
       );
     }
-    return firestore.collection("ykos_bbq_chicken").doc(user.uid);
-  }
-
-  //FAVORITES
-  Future<void> addFavorite(Food item) async {
-    try {
-      await userRef.collection('favorites').doc(item.id).set(item.toJson());
-    } on FirebaseException {
-      rethrow;
-    }
-  }
-
-  Future<void> removeFromFavorite(Food item) async {
-    try {
-      await userRef.collection('favorites').doc(item.id).delete();
-    } on FirebaseException {
-      rethrow;
-    }
-  }
-
-  Future<List<Food>> fetchFavorites() async {
-    try {
-      final snapshot = await userRef.collection("favorites").get();
-      return snapshot.docs.map((doc) => Food.fromJson(doc.data())).toList();
-    } on FirebaseException {
-      rethrow;
-    }
-  }
-
-  //ADRESS
-
-  //Add Adress
-  Future<void> addAdress(Adress item) async {
-    try {
-      await userRef.collection('adress').doc(item.id).set(item.toJson());
-    } on FirebaseException {
-      rethrow;
-    }
-  }
-
-  //Remove Adress
-  Future<void> removeFromAdress(Adress item) async {
-    try {
-      await userRef.collection('adress').doc(item.id).delete();
-    } on FirebaseException {
-      rethrow;
-    }
-  }
-
-  //Fetch Adress
-  Future<List<Adress>> fetchAdress() async {
-    try {
-      final snapshot = await userRef.collection("adress").get();
-      return snapshot.docs.map((doc) => Adress.fromJson(doc.data())).toList();
-    } on FirebaseException {
-      rethrow;
-    }
-  }
-
-  //Update Adress
-  Future<void> updateAdress(Adress updatedItem) async {
-    try {
-      await userRef
-          .collection('adress')
-          .doc(updatedItem.id)
-          .update(updatedItem.toJson());
-    } on FirebaseException {
-      rethrow;
-    }
+    return firestore.collection("ykos_kitchen").doc(user.uid);
   }
 
   //Order
-
-  //Add Order
-  Future<void> addOrder(Order item) async {
-    try {
-      await userRef.collection('orders').doc(item.orderId).set(item.toJson());
-    } on FirebaseException {
-      rethrow;
-    }
-  }
-
   //Fetch Orders
   Future<List<Order>> fetchOrders() async {
     try {
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:ykos_kitchen/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:ykos_kitchen/viewmodel/viewmodel_orders.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ViewmodelOrders())],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
